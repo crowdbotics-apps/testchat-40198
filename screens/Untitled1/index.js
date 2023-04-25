47295be1e2f86f70676ec619c6d3c3dbf490526f
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-25 10:12:28 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,15 +1,19 @@
+import { useNavigation } from "@react-navigation/native";
 import { Pressable } from "react-native";
 import { Text } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><Pressable><Text style={styles.SkmvDDxM}>Click to go to chat</Text></Pressable></ScrollView>
+    }}><Pressable onPress={() => {
+        navigation.navigate("Chat");
+      }}><Text style={styles.SkmvDDxM}>Click to go to chat</Text></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
